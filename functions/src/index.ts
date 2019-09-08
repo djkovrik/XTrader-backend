0f59d74ebd6dee77a07df11659e72f1a65026bd1
DJ_Kovrik <djkovrik@gmail.com>
2019-09-08 12:38:20 +0300

    refreshCurrencies func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as rp from 'request-promise';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// Call invocation url with key query parameter which contains coinmarketkap api key
+export const refreshCurrencies = functions.https.onRequest((request, response) => {
+
+    const apiKey = request.query.key;
+    
+    const options = {
+        uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map',
+        headers: {
+            'X-CMC_PRO_API_KEY': apiKey
+        },
+        json: true
+    };
+
+    rp(options)
+        .then((parsed) => {
+            response.send("Request completed, status: " + parsed.status.error_code);
+        })
+        .catch((err) => {
+            response.send("Error occurred: " + err.statusCode);
+        })
+});
