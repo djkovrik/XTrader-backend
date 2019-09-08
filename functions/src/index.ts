56225cbf38bfce5e3e7f0e1cda937633b0ce681c
DJ_Kovrik <djkovrik@gmail.com>
2019-09-08 15:35:40 +0300

    Basic request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,11 +4,6 @@ import * as rp from 'request-promise';
 
 admin.initializeApp();
 
-const FS_COLLECTION_NAME_SERVICE = 'service_info';
-const FS_DOC_NAME_LAST_UPDATED = 'last_updated';
-const FS_DOC_NAME_ERROR_MESSAGE = 'error_message';
-
-
 // Call invocation url with key query parameter which contains coinmarketkap api key
 export const refreshCurrencies = functions.https.onRequest((request, response) => {
 
@@ -24,32 +19,35 @@ export const refreshCurrencies = functions.https.onRequest((request, response) =
 
     rp(options)
         .then((parsed) => {
-            void saveTimestamp(parsed)
-            void saveErrorMessage(parsed);
-            
-            response.send("Request completed, status: " + parsed.status.error_code);
+            const error_code = parsed.status.error_code;
+            const error_message = parsed.status.error_message;
+            const timestamp = parsed.status.timestamp;
+
+            const data = parsed.data;
+            console.log("Request completed, status: " + parsed.status.error_code);
+
+            let message = ""
+            message += "Request completed:\n";
+            message += ` - error_code: ${error_code}\n`;
+            message += ` - error_message: ${error_message}\n`;
+            message += ` - timestamp: ${timestamp}\n`;
+            message += ` - First coin: ${data[0].name}\n`;
+            message += ` - Last coin: ${data[data.length - 1].name}\n`;
+
+            response.send(message);
         })
         .catch((err) => {
+            console.log("Error occurred: " + err.statusCode);
             response.send("Error occurred: " + err.statusCode);
         })
 });
 
-async function saveTimestamp(parsed: any) {
-    const timestamp = parsed.status.timestamp;
-    const tp = await admin.firestore()
-        .collection(FS_COLLECTION_NAME_SERVICE)
-        .doc(FS_DOC_NAME_LAST_UPDATED)
-        .set({ timestamp: timestamp });
+// async function saveData(parsed: any) {
+//     const timestamp = parsed.status.timestamp;
+//     const tp = await admin.firestore()
+//         .collection(FS_COLLECTION_NAME_SERVICE)
+//         .doc(FS_DOC_NAME_LAST_UPDATED)
+//         .set({ timestamp: timestamp }, { merge: true });
 
-    return tp
-}
-
-async function saveErrorMessage(parsed: any) {
-    const message = parsed.status.error_message;
-    const em = await admin.firestore()
-        .collection(FS_COLLECTION_NAME_SERVICE)
-        .doc(FS_DOC_NAME_ERROR_MESSAGE)
-        .set({ error_message: message });
-
-    return em
-}
+//     return tp
+// }
